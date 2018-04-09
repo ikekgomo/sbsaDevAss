d542f9ed5d1fb07101c370f0a8cea6c38d94ec17
A228724 <Itumeleng.kgomo@standardbank.co.za>
2018-04-09 21:07:38 +0200

    more

---
+++ A228724_MaximumTipCalculator/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author a228724
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
